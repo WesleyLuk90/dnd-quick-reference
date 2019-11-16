239238f1022b2468045b5e3ae384ea6a55e98318
Wesley Luk <wesleyluk@gmail.com>
2019-11-16 14:00:34 -0800

    Scroller

---
+++ src/components/Scroller.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./Scroller.css";
+
+interface ScrollerPosition {
+    scrollDistance: number;
+    height: number;
+}
+
+export function Scroller<T>({
+    elements,
+    render,
+    elementHeight
+}: {
+    elements: T[];
+    render: (t: T) => React.ReactNode;
+    elementHeight: number;
+}) {
+    const [position, setPosition] = useState<ScrollerPosition>({
+        scrollDistance: 0,
+        height: 1000
+    });
+
+    const scrollTarget = useRef<HTMLDivElement>(null);
+    const timeout = useRef<number | null>(null);
+    function updateScroll(scroller: HTMLDivElement | null | undefined) {
+        if (scroller == null) {
+            return;
+        }
+        setPosition({
+            scrollDistance: scroller.scrollTop,
+            height: scroller.clientHeight
+        });
+    }
+
+    useEffect(() => {
+        updateScroll(scrollTarget.current);
+    }, []);
+
+    function onScroll() {
+        if (timeout.current == null) {
+            timeout.current = requestAnimationFrame(() => {
+                timeout.current = null;
+                updateScroll(scrollTarget.current);
+            });
+        }
+    }
+
+    const before = Math.floor(position.scrollDistance / elementHeight);
+    const count = Math.ceil(position.height / elementHeight);
+    const after = elements.length - before - count;
+
+    return (
+        <div className="scroller" onScroll={onScroll} ref={scrollTarget}>
+            <div className="scroller__area">
+                <div
+                    className="scroller__top-padding"
+                    style={{ height: before * elementHeight }}
+                />
+                {elements.slice(before, before + count).map(render)}
+                <div
+                    className="scroller__bottom-padding"
+                    style={{
+                        height: after * elementHeight
+                    }}
+                />
+            </div>
+        </div>
+    );
+}
--- src/monster/MonsterList.tsx
+++ src/monster/MonsterList.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Scroller } from "../components/Scroller";
 import { Monster } from "../models/Monster";
 import { MonsterReference } from "../models/MonsterData";
 import { MonsterService } from "../services/MonsterService";
@@ -14,7 +15,6 @@ function alphabetic(a: Monster, b: Monster) {
 
 export function MonsterList() {
     const [monsters, setMonsters] = useState<Monster[] | null>(null);
-
     useEffect(() => {
         MonsterService.all().then(setMonsters);
     }, []);
@@ -22,26 +22,31 @@ export function MonsterList() {
     if (monsters == null) {
         return <span>Loading</span>;
     }
-
     return (
         <div className="monster-list">
             <div className="monster-list__list">
-                {monsters.sort(alphabetic).map(m => (
-                    <a
-                        className="monster-list__item"
-                        key={`${m.name} ${m.source}`}
-                        href={href(m)}
-                    >
-                        <div className="monster-list__name">{m.name}</div>
-                        <div className="monster-list__type">
-                            {m.type.format()}
-                        </div>
-                        <div className="monster-list__cr">
-                            {m.formatChallengeRating()}
-                        </div>
-                        <div className="monster-list__source">{m.source}</div>
-                    </a>
-                ))}
+                <Scroller
+                    elements={monsters.sort(alphabetic)}
+                    render={m => (
+                        <a
+                            className="monster-list__item"
+                            key={`${m.name} ${m.source}`}
+                            href={href(m)}
+                        >
+                            <div className="monster-list__name">{m.name}</div>
+                            <div className="monster-list__type">
+                                {m.type.format()}
+                            </div>
+                            <div className="monster-list__cr">
+                                {m.formatChallengeRating()}
+                            </div>
+                            <div className="monster-list__source">
+                                {m.source}
+                            </div>
+                        </a>
+                    )}
+                    elementHeight={32}
+                />
             </div>
         </div>
     );
