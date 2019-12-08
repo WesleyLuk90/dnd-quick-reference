e04f0074f59ba18e62ec6f2b1fa33addfc715487
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-08 21:30:56 +0000

    Deploy WesleyLuk90/dnd-quick-reference to github.com/WesleyLuk90/dnd-quick-reference.git:gh-pages

---
+++ precache-manifest.026094dd5bbb12d13be3ecaa6b77fbe8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b6e9cfd5970c887e6273cb54574f4e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "300e872fa95fb5241bff",
+    "url": "/static/css/main.7d9efc58.chunk.css"
+  },
+  {
+    "revision": "3572964c671d4278829d",
+    "url": "/static/js/2.998bb663.chunk.js"
+  },
+  {
+    "revision": "300e872fa95fb5241bff",
+    "url": "/static/js/main.2eeb9ca2.chunk.js"
+  },
+  {
+    "revision": "84a962ffeb5e194cbef9",
+    "url": "/static/js/runtime-main.f6a133ac.js"
+  }
+]);
